c9ac3765f0e9943b137f03de94c00a6b88bc134b
Showrin <showrinece15@gmail.com>
2021-05-16 00:07:02 +0600

    Send response to base route

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Server is running on port', port);
-})
+});
+
+app.get('/', (request, response) => {
+  response.status(200).json({
+    name: 'node-api-postgres',
+    version: '1.0.0',
+    description: 'Rest API and basic CRUD app using Node.JS, ExpressJS',
+    port,
+  });
+});
