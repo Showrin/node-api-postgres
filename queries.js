cfccb7b8b76f5d880ee82b917ec36842b4124fee
Showrin <showrinece15@gmail.com>
2021-05-17 01:57:52 +0600

    Write query function to fetch user by id

--- queries.js
+++ queries.js
@@ -11,4 +11,16 @@ const getAllUsers = (request, response) => {
   });
 };
 
-module.exports = { getAllUsers };
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, result) => {
+    if (error) {
+      response.status(500);
+    }
+
+    response.status(200).json(result.rows);
+  });
+};
+
+module.exports = { getAllUsers, getUserById };
