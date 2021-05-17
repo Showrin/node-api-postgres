47146ff1ef07fd08285d7d13376c6386e69b6d3c
Showrin <showrinece15@gmail.com>
2021-05-18 00:55:49 +0600

    Setup ssl config for production

--- pool.js
+++ pool.js
@@ -7,10 +7,16 @@ const connectionString =
   process.env.DATABASE_URL ||
   `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 
-console.log(connectionString);
-
-const pool = new Pool({
-  connectionString,
-});
+const pool =
+  process.env.NODE_ENV === 'production'
+    ? new Pool({
+        connectionString,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      })
+    : new Pool({
+        connectionString,
+      });
 
 module.exports = pool;
