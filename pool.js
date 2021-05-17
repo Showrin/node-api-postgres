c6b508ebc60102741fa2cb57360bf38d2b00ffc5
Showrin <showrinece15@gmail.com>
2021-05-17 22:30:49 +0600

    Use databse url from environment as connection string to connect postgresql db

--- pool.js
+++ pool.js
@@ -3,7 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connectionString = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+
+console.log(connectionString);
 
 const pool = new Pool({
   connectionString,
